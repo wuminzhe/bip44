0f6b6f18d8aa9e5998dd40d180c8fc0e24ac53d8
Wu Minzhe <wuminzhe@gmail.com>
2019-01-10 13:53:12 +0800

    small fix

--- lib/bip44/bitcoin.rb
+++ lib/bip44/bitcoin.rb
@@ -1,7 +1,8 @@
 module Bip44
   module Bitcoin
-    def bitcoin_address(network: :bitcoin)
-      @wallet_node.public_key.to_address(network: network)
+    def bitcoin_address(testnet: false)
+      return @wallet_node.public_key.to_address(network: :bitcoin_testnet) if testnet
+      @wallet_node.public_key.to_address
     end
   end
 end
